f6eecdbabfffd28396ff845d5385f321288132b1
Garrison Peacock <garrison.peacock@gmail.com>
2021-03-01 18:47:53 -0800

    Synced the demo code with the demo video

--- HLG-Arduino-Library.ino
+++ HLG-Arduino-Library.ino
@@ -9,6 +9,9 @@ HLG hlg;
 HLG::coord_t x;
 HLG::coord_t y;
 
+int ax;
+int ay;
+
 //
 // Runs once at startup
 //
@@ -21,6 +24,9 @@ setup()
 	// Set our (x, y) to (0, 0) -- the top left of the screen
 	x = 0;
 	y = 0;
+
+    ax = 10;
+    ay = 10;
 }
 
 //
@@ -32,36 +38,32 @@ loop()
     // Set the drawing color to pure red
     hlg.setColor(HLG::COLOR_RED);
 
-
     // Clear the screen
     hlg.clearScreen();
 
     // If our the right side of our rect to past the screen edge
     // reset the rect to (0, 0)
-    if ((x + 100) >= 1920)
+    if (((x + 100) >= 1920) || (x < 0))
     {
-    	x = 0;
-    	y = 0;
+    	ax *= -1;
     }
+
     // The the bottom of the rect is past the screen edge
     // reset the rect to (0, 0)
-    else if ((y + 150) >= 1080)
+    if (((y + 100) >= 1080) || (y < 0))
     {
-    	y = 0;
-    	x = 0;
+    	ay *= -1;
     }
 
     // Set the drawing color to pure blue
     hlg.setColor(HLG::COLOR_BLUE);
+
     // Draw our rectangle
-    hlg.drawRect(x, y, x + 100, y + 150);
+    hlg.drawRect(x, y, x + 100, y + 100);
 
     // Move the rectangle a bit to the right
-    x += 10;
+    x += ax;
 
     // Move the rect a bit down
-    y += 10;
-
-    // Delay for 100 ms
-    delay(1);
+    y += ay;
 }
